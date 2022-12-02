1bd2727a32e4de9b167673a0eaea2b7f879b28d4
Andrew Cox <18695702+Cox-Andrew@users.noreply.github.com>
2022-12-02 00:51:47 +0000

    add varaible avatar size

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -2,14 +2,16 @@ import Image from "next/image";
 import type { User } from "@prisma/client";
 import CryptoJS from "crypto-js";
 
-const Avatar = ({ user }: { user: User | null }) => {
+const Avatar = ({ user, size=20 }: { user: User | null, size?: number }) => {
   const emailHash = user ? CryptoJS.MD5(user?.email || "").toString() : "";
 
   return (
     <Image
-      className="h-5 w-5 rounded-full"
-      src={user ? `https://www.gravatar.com/avatar/${emailHash}` : "/robot.png"}
+      className={`rounded-full`}
+      src={user ? `https://www.gravatar.com/avatar/${emailHash}?s=${size}&d=identicon` : "/robot.png"}
       alt={user?.name || "A picture of a robot"}
+      width={size}
+      height={size}
     />
   );
 };
